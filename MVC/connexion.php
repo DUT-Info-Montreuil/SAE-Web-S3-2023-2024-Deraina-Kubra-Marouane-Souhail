f2c0b662093b9566c89588c6c56bd77930ba4446
sirhbira <souhail.irb28@gmail.com>
2023-12-11 12:20:35 +0100

    la version bonne

--- MVC/connexion.php
+++ MVC/connexion.php
@@ -3,7 +3,7 @@ class Connexion {
 	protected static $bdd;
 	
 	public function init_connexion() {
-		$dsn = "mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw201699";
+		$dsn = "mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw20164";
 		$user = "dutinfopw20164";
 		$password = "rytutyta";
 		self::$bdd = new PDO ($dsn, $user, $password);
