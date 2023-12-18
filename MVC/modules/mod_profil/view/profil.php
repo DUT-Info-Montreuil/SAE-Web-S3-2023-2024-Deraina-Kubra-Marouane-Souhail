8374cfc8afc99cdb4a1d242583b4f9855db0b2b4
KubraGu <kubra.gulsumabur@gmail.com>
2023-12-18 14:45:07 +0100

    design de la page

--- MVC/modules/mod_profil/view/profil.php
+++ MVC/modules/mod_profil/view/profil.php
@@ -12,7 +12,7 @@
     border-radius: 10px;
     cursor: pointer;
     font-family: 'Julius Sans One', sans-serif;
-    margin-bottom: 10px; /* Espacement entre les boutons */
+    margin-bottom: 10px; 
   }
 
 /* Conteneur principal */
@@ -25,33 +25,81 @@
   padding: 20px; 
 }
 
-.left-buttons {
+.button-groups {
   display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  margin-bottom: 20px; 
+  justify-content: space-between;
+  width: 100%;
+  gap: 10px;
 }
 
-
+.left-buttons,
 .right-buttons {
+  flex: 1;
   display: flex;
+  gap: 10px;
   flex-direction: column;
-  align-items: flex-start;
-  margin-bottom: 20px; 
 }
 
 .bottom-left {
-  align-self: flex-start;
+  
+  margin-top: 100px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
+
+.info-blocks {
+    display: flex;
+    justify-content: space-between;
+    /* Ajoutez d'autres styles si nécessaire */
+}
+.text-tournoi .tournament-info {
+  font-size: 25px;
+  color: #000000;
+  font-family: 'Julius Sans One', sans-serif;
+}
+
+.text-tournoi {
+  position: absolute;
+  bottom:10%;
+}
+
+.text-score .score-info {
+  font-size: 25px;
+  color: #000000;
+  font-family: 'Julius Sans One', sans-serif;
+}
+
+.text-score{
+  position: absolute;
+  top:20%;
+  margin-top:10px;
+  right: 20%;
+}
+
 p {
     font-family: 'Inknut Antiqua', sans-serif;
     font-weight: bold;
     font-size: 30px;
+    text-align: left;
+    margin-left: 20;
   }
+
+.blue-squares-container {
+  margin-left: 20px;
+  margin-top: 20px;
+  display: flex;
+  gap: 15px;
+  justify-content: flex-start;
+  margin-bottom: 20px;
+}
+
+
 .blue-square {
     position: relative;
     width: 100px;
-    height: 150px;
+    height: 100px;
     background-color: #ADD8E6;
     display: flex;
     justify-content: center;
@@ -60,7 +108,14 @@ p {
     font-weight: bold;
     color: #006ED3;
     border-radius: 10px;
+    margin-right: 20px;
 }
+
+
+.blue-square:last-child {
+    margin-right: 0; 
+}
+
 .vertical-rectangle {
       width: 5px;
       height: 40px;
@@ -71,23 +126,116 @@ p {
     position: absolute;
     top: 12px;
     font-size: 12px;
-    color: #696969;
+    color: white;
     font-family: 'Manrope', sans-serif;
+}
+
+.bottom-blue-square {
+    position: relative;
+    justify-content: center;
+    align-items: center;
+}
+
+.bottom-blue-square .number {
+    position: relative; /* Change la position à relative */
+    z-index: 1;
+    text-align: center; /* Centre le texte à l'intérieur */
+    margin-top: 10px; /* Ajuste la marge au besoin */
+}
+
+.bottom-blue-square::after {
+    content: "";
+    background: url('https://api.iconify.design/wpf/coins.svg?color=gold') no-repeat center center / contain;
+    position: absolute;
+    bottom: -15px; 
+    left: 50%;
+    transform: translateX(-50%);
+    width: 50px;
+    height: 50px;
+    z-index: 0;
+}
+
+.rectangleReussite {
+    width: 150px;
+    height: 80px;
+    background-color: #006ED3;
+    color: white;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 20px;
+    border-radius: 10px;
+    position: relative;
+    overflow: hidden;
+}
+
+.text-reu {
+    font-weight: bold; 
+}
+
+.rectangleReussite::before,
+.rectangleReussite::after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    background: linear-gradient(45deg, transparent 33.33%, rgba(255, 255, 255, 0.4) 33.33%, rgba(255, 255, 255, 0.4) 66.66%, transparent 66.66%);
+}
+
+.rectangleReussite::before {
+    left: -50%;
+    transform: skewX(-30deg);
+}
+
+.rectangleReussite::after {
+    right: -50%;
+    transform: skewX(30deg);
 }
 
 </style>
 
 <body>
-    
-    <div class="blue-square">
-    <div class="vertical-rectangle"></div>
-    <div class="text-above">Partie Gagnée</div>
-    30&nbsp;
+
+<body>  
+
+    <div class="blue-squares-container">
+        <div class="blue-square">
+            <div class="vertical-rectangle"></div>
+            <div class="text-above">Partie Gagnée</div>
+            30
+        </div>
+
+        <div class="blue-square">
+            <div class="vertical-rectangle"></div>
+            <div class="text-above">Partie Jouées</div>
+            30
+        </div>
+
+        <div class="blue-square">
+            <div class="vertical-rectangle"></div>
+            <div class="text-above">Partie Perdues</div>
+            30
+        </div>
+
+        <div class="blue-square bottom-blue-square">
+        <div class="vertical-rectangle"></div>
+            <div class="text-above">Argent</div>
+            30
+        </div>
+
+        <div class="rectangleReussite">
+          <div class="text-reu">Réussite</div>
+          %
+        </div>
     </div>
-    
-    <p>COMMUNAUTÉ</p>
+
+    <p>COMMUNAUTÉ :</p>
 
     <div class="container">
+      <div class="button-groups">
         <div class="left-buttons">
         <button class="custom-button">Envoyer un message</button>
         <button class="custom-button">Envoyer de l'argent</button>
@@ -100,5 +248,21 @@ p {
         <div class="bottom-left">
         <button class="custom-button">Prendre une mission</button>
         </div>
+      </div>
+        <div class="info-blocks">
+            <div class="text-score">
+                <p class="score-info">Classement :</p><br></br>
+                <p class="score-info">Score :</p>
+            </div>
+
+            <div class="text-tournoi">
+                <p class="tournament-info">A participé à</p>
+                <p class="tournament-info">tournois</p><br></br>
+                <p class="tournament-info">A créé</p>
+                <p class="tournament-info">tournois</p>
+            </div>
+        </div>
     </div>
+
+
 </body>
