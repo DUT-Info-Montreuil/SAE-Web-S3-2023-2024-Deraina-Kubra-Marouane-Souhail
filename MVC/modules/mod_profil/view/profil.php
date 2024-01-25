90f655ded418ea0c361f2c1f899f7b3285eeb174
mirhbira <irhbiram@gmail.com>
2024-01-25 15:15:40 +0100

    bonne version

--- MVC/modules/mod_profil/view/profil.php
+++ MVC/modules/mod_profil/view/profil.php
@@ -19,7 +19,7 @@
             right: 0;
             bottom: 0;
             z-index: -1; /* Placez le pseudo-élément en arrière-plan */
-            background: url('././Images/fond1.avif') no-repeat center center fixed; 
+            background: url('././Images/fond.avif') no-repeat center center fixed; 
             background-size: cover;
             filter: blur(5px); /* Réglez ceci pour l'intensité du flou */
         }
